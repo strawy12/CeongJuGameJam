bee307690e6383ec256937c48fc12bdad81d0286
wjdaudwlr <jmj51755245@gmail.com>
2022-08-21 08:01:22 +0900

    back button

--- Assets/02.Scripts/UI/GameOverUI.cs
+++ Assets/02.Scripts/UI/GameOverUI.cs
@@ -5,31 +5,5 @@ using TMPro;
 
 public class GameOverUI : MonoBehaviour
 {
-    public GameObject gameoverPanel;
-
-    public Timer timer;
-    public TMP_Text scoreText;
-
-
-    private void Start()
-    {
-        EventManager.StartListening("GameOver", GameOver);
-    }
-
-    private void GameOver()
-    {
-        gameoverPanel.SetActive(true);
-
-        scoreText.text = timer._timerText.text;
-    }
-
-    private void OnDestroy()
-    {
-        EventManager.StopListening("GameOver", GameOver);
-    }
-
-    private void OnApplicationQuit()
-    {
-        EventManager.StopListening("GameOver", GameOver);
-    }
+    
 }
