0a25ad1b6642087da32bee4b607482cdd5537adf
strawy12 <hwaga0714@gmail.com>
2022-08-21 03:22:11 +0900

    병합 완료

--- Assets/02.Scripts/Player.cs
+++ Assets/02.Scripts/Player.cs
@@ -11,14 +11,7 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-<<<<<<< HEAD
-        
-        currentHp = maxHp;
-=======
         EventManager.StartListening("GameStart", InitHp);
->>>>>>> 14abbdfdf941bf6f1b83f4b56e317630e48741cb
-
-        hpBar.SetMaxHealth(maxHp);
     }
 
     private void InitHp()
